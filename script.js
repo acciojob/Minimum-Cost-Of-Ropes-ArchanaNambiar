80f4bcbc7acc57e4967eb7a624c6f5ea883502fb
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-07-18 14:59:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,3 +29,13 @@ console.log(total);
 let result=document.getElementById('result');
 result.innerHTML=total;
 }
+function compare(a, b) {
+  if (a < b) {
+      return -1;
+  } else if (a > b) {
+      return 1;
+  } else {
+      return 0;
+  }
+}
+ 
