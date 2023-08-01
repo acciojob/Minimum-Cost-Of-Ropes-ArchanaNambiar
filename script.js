2dbb5666d7ffd90aab7af66ff48056d96914b5ad
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-08-01 20:31:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,86 +1,42 @@
-
-function minCostOfRopes(lengths) {
-  // Convert the input string to an array of integers
-  const ropes = lengths.split(',').map(str => parseInt(str.trim()));
-
-  // Create a min-heap using priority queue
-  const heap = new MinHeap();
-  ropes.forEach(length => heap.insert(length));
-
-  let totalCost = 0;
-
-  // While there are more than one rope in the heap
-  while (heap.size() > 1) {
-    // Extract the two shortest ropes from the heap
-    const rope1 = heap.extractMin();
-    const rope2 = heap.extractMin();
-
-    // Connect the two ropes and add the cost to the total
-    const combinedLength = rope1 + rope2;
-    totalCost += combinedLength;
-
-    // Insert the result back into the heap
-    heap.insert(combinedLength);
-  }
-
-  // Return the total minimum cost
-  return totalCost;
-}
-
-// Sample input string provided in the problem statement
-const inputString = '4, 2, 7, 6, 9';
-const minCost = minCostOfRopes(inputString);
-document.getElementById('result').innerText = minCost;
-
-// MinHeap implementation for priority queue
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  insert(value) {
-    this.heap.push(value);
-    this.bubbleUp(this.heap.length - 1);
-  }
-
-  bubbleUp(index) {
-    const parentIdx = Math.floor((index - 1) / 2);
-    if (parentIdx >= 0 && this.heap[parentIdx] > this.heap[index]) {
-      [this.heap[parentIdx], this.heap[index]] = [this.heap[index], this.heap[parentIdx]];
-      this.bubbleUp(parentIdx);
-    }
-  }
-
-  extractMin() {
-    if (this.heap.length === 0) return null;
-    if (this.heap.length === 1) return this.heap.pop();
-
-    const minValue = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this.bubbleDown(0);
-    return minValue;
-  }
-
-  bubbleDown(index) {
-    const leftChildIdx = 2 * index + 1;
-    const rightChildIdx = 2 * index + 2;
-    let smallestIdx = index;
-
-    if (leftChildIdx < this.heap.length && this.heap[leftChildIdx] < this.heap[smallestIdx]) {
-      smallestIdx = leftChildIdx;
-    }
-
-    if (rightChildIdx < this.heap.length && this.heap[rightChildIdx] < this.heap[smallestIdx]) {
-      smallestIdx = rightChildIdx;
-    }
-
-    if (smallestIdx !== index) {
-      [this.heap[index], this.heap[smallestIdx]] = [this.heap[smallestIdx], this.heap[index]];
-      this.bubbleDown(smallestIdx);
-    }
+function compare(a, b) {
+  if (a < b) {
+      return -1;
+  } else if (a > b) {
+      return 1;
+  } else {
+      return 0;
   }
 }
+ 
+ 
+ 
+function calculateMinCost() {
+  //your code here
+  let str=document.getElementById('rope-lengths').value.split(",");
+  let arr=str.map((str)=>parseInt(str));
+  // console.log(arr);
+  
+let total=0;
+arr=arr.sort(compare);
+// console.log("arr ",arr);
+while(arr.length>=2){
+  let sum=arr[0]+arr[1];
+  // console.log(sum);
+  let rem=[];
+  rem.push(sum);
+  for(let k=2;k<arr.length;k++){
+    rem.push(arr[k]);
+  }
+  rem=rem.sort(compare);
+  // console.log("rem ",rem);
+  arr=[...rem];
+  // console.log("arr ",arr);
+  total=total+sum;
+  // console.log("total "+total+"----------------------");
+}
+console.log(total);
+ 
+ 
+let result=document.getElementById('result');
+result.innerHTML=total;
+}  
